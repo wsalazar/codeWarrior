bdd6d3f114c2aac66aab2ccd4d2793455329bd61
Adam Cortez <xwing@xwing-vostro-1400.(none)>
2012-07-16 22:31:37 -0400

    New Quad Example

--- quadfunction.php
+++ quadfunction.php
@@ -7,12 +7,7 @@ function quad ($num)
 	$disc = pow($num[1],2)-4*$num[0]*$num[2];
 	if($disc<0)
 	{
-	  $disc = sqrt(-1*$disc);
-	  $real1 = -$num[1]/(2*$num[0]);
-	  $imag1 = $disc/(2*$num[0]);
-	  $imag2 = -$disc/(2*$num[0]);
-	  echo $real1.$imag1."j";
-	  echo $real1.$imag2."j";
+	    quad3($disc,$num);
 	}
 	else
 	{
@@ -25,8 +20,8 @@ function quad2($disc,$num)
 	$disc=Sqrt($disc);
 	$root1=($num[1]+$disc)/(2*$num[0]);
 	$root2=(-$num[1]-$disc)/(2*$num[0]);
-    echo $root1;
-	echo $root2;
+    echo $root1.PHP_EOL.$root2.PHP_EOL;
+	
 	
 	if($num[0]<0)
 	{
@@ -37,6 +32,16 @@ function quad2($disc,$num)
 		echo "Opens Up";
 	}
 }
+
+function quad3 ($disc,$num)
+{
+	$disc = sqrt(-1*$disc);
+	$real1 = -$num[1]/(2*$num[0]);
+	$imag1 = $disc/(2*$num[0]);
+	$imag2 = -$disc/(2*$num[0]);
+	echo $real1.$imag1."j".PHP_EOL. $real1.$imag2."j";
+}
+	  
 
 quad($num);
 
